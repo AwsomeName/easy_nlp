c9702db3a37c50e3d5113730ffe8c499f7adee24
liunux1992 <liunux1992@gmail.com>
2023-04-27 01:30:55 +0000

    easy_gpt2

--- finetune_gpt2.py
+++ finetune_gpt2.py
@@ -24,81 +24,8 @@ import os
 import torch
 from transformers.trainer import TRAINING_ARGS_NAME
 
-class FinetuneTrainer(Trainer):
-    def compute_loss(self, model, inputs, return_outputs=False):
-        return model(
-            input_ids=inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
-            position_ids=inputs['position_ids'],
-            labels=inputs['labels'],
-        ).loss
-        
-    def save_model(self, output_dir=None, _internal_call=False, lora_name="adapter_model.bin"):
-        os.makedirs(output_dir, exist_ok=True)
-        torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
-        saved_params = {
-            k: v.to("cpu") for k, v in self.model.named_parameters() if v.requires_grad
-        }
-        # return super().save_model(output_dir, _internal_call)
-        torch.save(saved_params, os.path.join(output_dir, lora_name))
-    
-    
-def train_or_pred(
-        train_on_inputs: bool = True, # if False, masks out inputs in loss
-    ):
-    
-    def tokenize(prompt, add_eos_token=True):
-        result = tokenizer(
-            prompt,
-            truncation=True,
-            max_length=args.max_length,
-            padding=True,
-            return_tensors=None,
-        )
-    
-        if (
-            result["input_ids"][-1] != tokenize.eos_token_id
-            and len(result['input_ids']) < args.max_length
-            and add_eos_token
-        ):
-            result["input_ids"].append(tokenizer.eos_token_id)
-            result["attention_mask"].append(1)
-            
-        if add_eos_token and len(result["input_ids"]) >= args.max_length:
-            result["input_ids"][args.max_length - 1] = tokenizer.eos_token_id
-            result['attention_mask'][args.max_length - 1] = 1
-            
-        result["labels"] = result["input_ids"].copy()
-        
-        return result
-    
-    def generate_and_tokenize_prompt(data_point):
-        instruction = data_point['instruction']
-        input_text = data_point["input"]
-        input_text = "Human: " + instruction + input_text + "\n\nAssistant: "
-        # print("---------")
-        # print(input_text)
-        input_text = tokenizer.bos_token + input_text if tokenizer.bos_token != None else input_text
-        target_text = data_point["output"] + tokenizer.eos_token
-        full_prompt = input_text + target_text
-        # print(full_prompt)
-        # print("---------1-")
-        tokenized_full_prompt = tokenize(full_prompt)
-        # print(tokenized_full_prompt)
-        # print("----------")
-        if not train_on_inputs:
-            user_prompt = input_text
-            tokenized_user_prompt = tokenize(user_prompt, add_eos_token=False)
-            user_prompt_len = len(tokenized_user_prompt["input_ids"])
-            
-            tokenized_full_prompt["labels"] = [
-                -100
-            ] * user_prompt_len + tokenized_full_prompt["labels"][
-                user_prompt_len:
-            ]
-            
-        return tokenized_full_prompt
-    
+
+def train_or_pred():
     logger.info(args)
     logger.info("token...")
     tokenizer = AutoTokenizer.from_pretrained(args.pre_train, padding_side="left")
@@ -110,30 +37,14 @@ def train_or_pred(
     logger.info("model done")
     
     # logger.info("train data ...")
-    # train_dataset = TextDataset(tokenizer=tokenizer, file_path=args.train_file, block_size=128)
+    train_dataset = TextDataset(tokenizer=tokenizer, file_path=args.train_file, block_size=128)
     # logger.info("test data...")
     test_dataset = TextDataset(tokenizer=tokenizer, file_path=args.test_file, block_size=128)
     
     # logger.info("colla data...")
-    # data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
     
     logger.info("train data ...")
-    data = load_dataset("json", data_files=args.train_file)
-    # print(type(data))
-    # print(data)
-    # print("-------")
-    # exit()
-    train_val = data["train"].train_test_split(
-        test_size=0.1, shuffle=True, seed=42
-    )
-    # print(train_val)
-    # print("--------")
-    # train_dataset = train_val['train'].shuffle()
-    train_dataset = train_val['train'].shuffle().map(generate_and_tokenize_prompt)
-    test_dataset = train_val['test'].shuffle().map(generate_and_tokenize_prompt)
-    data_collator = transformers.DataCollatorForSeq2Seq(
-        tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
-    )
     
     training_args = TrainingArguments(
         output_dir=args.output_dir,
@@ -158,7 +69,6 @@ def train_or_pred(
     )
     
     logger.info("Trainer...")
-    # data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
     if True:
         trainer = Trainer(
             model=model,
@@ -169,19 +79,10 @@ def train_or_pred(
         )
     
         logger.info("start...")
-        # trainer.train()
-        # (global_step, training_loss, metrics) = trainer.train()
         
         # model.config.use_cache = False
-        # if args.use_lora:
-        #     old_state_dict = model.state_dict
-        #     model.state_dict = (
-        #         lambda self, *_, **__: get_peft_model_state_dict(self, old_state_dict())
-        #     ).__get__(model, type(model))
-        
         # if torch.__version__ >= "2" and sys.platform != "win32":
         #     model = torch.compile(model)
-        # logger.info("trainer.train")
         (global_step, training_loss, metrics) = trainer.train(resume_from_checkpoint = args.resume_from_checkpoint)
         logger.info("Save checkpointing ...")
         
@@ -193,8 +94,8 @@ def train_or_pred(
         
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--train_file", default="data/instruct_train_1k.txt", type=str)
-    # parser.add_argument("--train_file", default="data/train_100.txt", type=str)
+    # parser.add_argument("--train_file", default="data/instruct_train_1k.txt", type=str)
+    parser.add_argument("--train_file", default="data/train_100.txt", type=str)
     parser.add_argument("--test_file", default="data/test_10.txt", type=str)
     parser.add_argument("--model_type", default="gpt2", type=str)
     parser.add_argument("--model_name", default="sshleifer/tiny-gpt2", type=str)
