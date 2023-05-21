e54cdae1487cfa647008c1af7a542049dc4fb3fa
AwsomeName <liuchang.hit.cs@gmail.com>
2023-05-21 23:38:28 +0800

    update

--- trl/install.sh
+++ trl/install.sh
@@ -1,4 +1,5 @@
 pip install git+https://github.com/huggingface/transformers
+pip install dataset
 pip install git+https://github.com/huggingface/peft.git -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install trl
 pip install bitsandbytes loralib
