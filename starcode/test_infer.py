0c18337d5d1765e96c020f8699269d3340249328
AwsomeName <liuchang.hit.cs@gmail.com>
2023-05-21 21:12:51 +0800

    添加auth

--- starcode/test_infer.py
+++ starcode/test_infer.py
@@ -4,8 +4,10 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 checkpoint = "bigcode/starcoder"
 device = "cuda" # for GPU usage or "cpu" for CPU usage
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint).to(device)
+acc_token = "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"
+
+tokenizer = AutoTokenizer.from_pretrained(checkpoint, use_auth_token=acc_token)
+model = AutoModelForCausalLM.from_pretrained(checkpoint, use_auth_token=acc_token).to(device)
 
 inputs = tokenizer.encode("def print_hello_world():", return_tensors="pt").to(device)
 outputs = model.generate(inputs)
