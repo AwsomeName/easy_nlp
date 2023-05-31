42cb593ce5d4e8c6d9e7853dd0e4af373b432e73
AwsomeName <liuchang.hit.cs@gmail.com>
2023-05-31 18:01:25 +0800

    update

--- starcode/utils/code_test.py
+++ starcode/utils/code_test.py
@@ -1,2 +1,14 @@
-from datasets import load_dataset
-load_dataset("openai_humaneval")
+from datasets import load_dataset, load_from_disk
+test_data = load_dataset("openai_humaneval")
+test_data.save_to_disk('/root/data/humaneval')
+
+# test_data = load_from_disk('/root/data/humaneval')
+
+# DatasetDict({
+#     test: Dataset({
+#         features: ['task_id', 'prompt', 'canonical_solution', 'test', 'entry_point'],
+#         num_rows: 164
+#     })
+# })
+
+print(test_data)
