44c7e190b4ac445dc8795192cb5f1b8a72f231c7
AwsomeName <liuchang.hit.cs@gmail.com>
2024-11-03 00:41:30 +0800

    update

---
+++ utils/change_st2st.py
@@ -0,0 +1,21 @@
+import torch
+import safetensors.torch
+
+from safetensors.torch import load_file, save_file
+import torch
+
+from_path = "/hy-tmp/Qwen2.5-0.5B-Instruct/model.safetensors"
+safetensors_path = "/hy-tmp/YJT2.5-0.5B-Instruct/model.safetensors"
+
+tensors_dict = load_file(from_path)
+
+
+# def convert_bin_to_safetensors(bin_path, safetensors_path):
+#     # 加载bin文件
+#     state_dict = torch.load(bin_path, map_location="cpu")
+    
+safetensors.torch.save_file(tensors_dict, safetensors_path)
+    
+
+
+# convert_bin_to_safetensors(bin_path, safetensors_path)
